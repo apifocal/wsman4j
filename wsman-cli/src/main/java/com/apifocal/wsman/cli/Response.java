07c7da545c452e77df83a98a51e38ce57ac27011
Ciprian Ciubotariu <cheepeero@gmx.net>
2015-12-02 01:39:06 +0200

    Remote @author javadoc tag and license stub

--- wsman-cli/src/main/java/com/apifocal/wsman/cli/Response.java
+++ wsman-cli/src/main/java/com/apifocal/wsman/cli/Response.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.apifocal.wsman.cli;
 
 /**
- *
- * @author chris
- * 
- * output of an executed wsman command
+ * Output of an executed wsman command.
  */
 public class Response {
     public int statusCode;
