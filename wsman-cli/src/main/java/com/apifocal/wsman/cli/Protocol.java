4432c71f3285570ad14f2e44c4184632cdc88cac
kcris <cristian.tarsoaga@gmail.com>
2015-12-07 00:45:55 +0200

    -

--- wsman-cli/src/main/java/com/apifocal/wsman/cli/Protocol.java
+++ wsman-cli/src/main/java/com/apifocal/wsman/cli/Protocol.java
@@ -26,7 +26,11 @@ public class Protocol {
     }
 
     int openShell() {
-//        //add SOAP headers, @see http://www.sachinhandiekar.com/2014/02/setting-soap-headers-in-apache-cxf.html
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //wsmanService.create(body);
+
+        
+//        //add SOAP headers
 //        List<Header> headersList = new ArrayList<>();
 ////        headersList.add(
 ////            new Header(new QName("http://sachinhandiekar.com/ws/SampleWS", "userName"), "JohnDoe", new JAXBDataBinding(String.class))
@@ -34,30 +38,33 @@ public class Protocol {
 //        Client proxy = ClientProxy.getClient(wsmanService);
 //        proxy.getRequestContext().put(Header.HEADER_LIST, headersList);
 //
-//        //invoke wsman service's 'create'
-//        //Shell shell = new Shell();
+//        //add SOAP body
 //        AnyXmlType body = new AnyXmlType();
+//        //Shell shell = new Shell();
 //        //body.setAny(shell);
-//        wsmanService.create(body); //ws call        
+//
+//        //invoke 'create' ws
 //
 //        return 0;
-
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     int runCommand(int shellId, String command, String[] args) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //wsmanService.command(body);
     }
 
     Response getCommandOutput(int shellId, int commandId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //wsmanService.receive(body);
     }
 
     void cleanupCommand(int shellId, int commandId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //wsmanService.signal(body);
     }
 
     void closeShell(int shellId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //wsmanService.delete();
     }    
 }
